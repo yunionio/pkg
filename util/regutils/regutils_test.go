ee0ffefae2fa988dda9d71ca5d0b2e39b789bea4
Qiu Jian <qiujian@yunionyun.com>
2018-10-20 11:15:43 +0800

    增加校验currency数字的regutils

--- util/regutils/regutils_test.go
+++ util/regutils/regutils_test.go
@@ -20,3 +20,44 @@ func TestIsFunction(t *testing.T) {
 	}
 
 }
+
+func TestRegexp(t *testing.T) {
+	cases := []struct {
+		in       string
+		funcName string
+		regfunc  func(str string) bool
+		want     bool
+	}{
+		{"3.490.000,89", "MatchEUCurrency", MatchEUCurrency, true},
+		{"3.000,89", "MatchEUCurrency", MatchEUCurrency, true},
+		{"3.490", "MatchEUCurrency", MatchEUCurrency, true},
+		{"0,89", "MatchEUCurrency", MatchEUCurrency, true},
+		{"3.490.000,", "MatchEUCurrency", MatchEUCurrency, true},
+		{"3,490,000,", "MatchEUCurrency", MatchEUCurrency, false},
+
+		{"3,490,000,", "MatchUSCurrency", MatchUSCurrency, false},
+		{"3,490,000.", "MatchUSCurrency", MatchUSCurrency, true},
+		{"3,490,000.89", "MatchUSCurrency", MatchUSCurrency, true},
+		{"3,000.89", "MatchUSCurrency", MatchUSCurrency, true},
+		{"3,490", "MatchUSCurrency", MatchUSCurrency, true},
+		{"0.89", "MatchUSCurrency", MatchUSCurrency, true},
+		{"3.490.000,89", "MatchUSCurrency", MatchUSCurrency, false},
+		{"3.000,89", "MatchUSCurrency", MatchUSCurrency, false},
+		{"3.490", "MatchUSCurrency", MatchUSCurrency, true},
+		{"0,89", "MatchUSCurrency", MatchUSCurrency, false},
+		{"3.490.000,", "MatchUSCurrency", MatchUSCurrency, false},
+		{"3.490.000.", "MatchEUCurrency", MatchEUCurrency, false},
+
+		{"10.168.222.23", "MatchIPAddr", MatchIPAddr, true},
+		{"10.168.222.", "MatchIPAddr", MatchIPAddr, false},
+
+		{"1.0.168.192.in-addr.arpa", "MatchPtr", MatchPtr, true},
+		{"0.168.192.in-addr.arpa", "MatchPtr", MatchPtr, false},
+	}
+	for _, c := range cases {
+		got := c.regfunc(c.in)
+		if got != c.want {
+			t.Errorf("%s(%s) != %v", c.funcName, c.in, c.want)
+		}
+	}
+}
