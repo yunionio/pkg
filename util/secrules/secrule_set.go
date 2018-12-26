f92351937f1ebc803096881b75814b2d15dd7206
Yousong Zhou <zhouyousong@yunionyun.com>
2018-12-26 09:34:25 +0000

    secrules: SecurityRuleSet: 改正Less定义

--- util/secrules/secrule_set.go
+++ util/secrules/secrule_set.go
@@ -1,9 +1,5 @@
 package secrules
 
-import (
-	"strings"
-)
-
 type SecurityRuleSet []SecurityRule
 
 func (v SecurityRuleSet) Len() int {
@@ -18,7 +14,7 @@ func (v SecurityRuleSet) Less(i, j int) bool {
 	if v[i].Priority > v[j].Priority {
 		return true
 	} else if v[i].Priority == v[j].Priority {
-		return strings.Compare(v[i].String(), v[j].String()) <= 0
+		return v[i].String() < v[j].String()
 	}
 	return false
 }
