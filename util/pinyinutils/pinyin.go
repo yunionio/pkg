7af3232d9d44f0922aebf8d416fcff1b083581e6
马鸿飞 <mahongfei@yunion.cn>
2023-04-19 10:56:06 +0800

    fix(pinyinutils): 添加获取汉字拼音首字母方法

--- util/pinyinutils/pinyin.go
+++ util/pinyinutils/pinyin.go
@@ -15,11 +15,13 @@
 package pinyinutils
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/mozillazg/go-pinyin"
 )
 
+// 获取汉字拼音全拼
 func Text2Pinyin(hans string) string {
 	args := pinyin.NewArgs()
 	out := strings.Builder{}
@@ -34,4 +36,19 @@ func Text2Pinyin(hans string) string {
 		}
 	}
 	return out.String()
+}
+
+// 获取汉字拼音首字母
+func Text2FirstPinyin(hans string) string {
+	a := pinyin.NewArgs()
+	rows := pinyin.Pinyin(hans, a)
+	strResult := ""
+	for i := 0; i < len(rows); i++ {
+		if len(rows[i]) != 0 {
+			str := rows[i][0]
+			pi := str[0:1]
+			strResult += string(bytes.ToUpper([]byte(pi)))
+		}
+	}
+	return strResult
 }
